edd4f1f75622a47ec3f7827a3131e2a820b034bf
Olisaurio <santioro1736@gmail.com>
2024-07-16 19:01:12 -0500

    crar dev

---
+++ src/main/java/com/petShop/HolaMundoController.java
@@ -0,0 +1,20 @@
+package com.petShop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+
+public class HolaMundoController {
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Hola Mundo";
+    }
+
+    @GetMapping("/chao")
+    public String adios(){
+        return "Adios mundo";
+    }
+}
