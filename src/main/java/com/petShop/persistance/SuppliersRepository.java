079e8f55d6c2eced993a816e548a16a25516dc4f
Olisaurio <santioro1736@gmail.com>
2024-09-17 18:35:34 -0500

    create clases SuppliersController

--- src/main/java/com/petShop/persistance/SuppliersRepository.java
+++ src/main/java/com/petShop/persistance/SuppliersRepository.java
@@ -3,29 +3,31 @@ package com.petShop.persistance;
 import com.petShop.persistance.crud.SuppliersCrudRepository;
 import com.petShop.persistance.entity.Quotes;
 import com.petShop.persistance.entity.Suppliers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class SuppliersRepository {
 
-    SuppliersCrudRepository SuppliersCrudRepository;
+    @Autowired
+    private SuppliersCrudRepository suppliersCrudRepository;
 
     public List<Suppliers> getAll(){
-        return (List<Suppliers>) SuppliersCrudRepository.findAll();
+        return (List<Suppliers>) suppliersCrudRepository.findAll();
     }
 
-
-    public Optional<Suppliers> getPetById(int id) {
-        return SuppliersCrudRepository.findById(id);
+    public Optional<Suppliers> getSupplierById(int id) {
+        return suppliersCrudRepository.findById(id);
     }
 
-    public Suppliers save(Suppliers Suppliers) {
-        return SuppliersCrudRepository.save(Suppliers);
+    public Suppliers save(Suppliers supplier) {
+        return suppliersCrudRepository.save(supplier);
     }
 
     public void delete(int id) {
-        SuppliersCrudRepository.deleteById(id);
+        suppliersCrudRepository.deleteById(id);
     }
-
 }
